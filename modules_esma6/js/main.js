a1abf9836f9f76e45878c200007afe3b30bc4105
miloskuzmic <miloskuzmic@gmail.com>
2021-07-13 19:21:20 +0200

    Rename of folder

--- modules_esma6/js/main.js
+++ modules_esma6/js/main.js
@@ -1,6 +1,6 @@
-import addMovieToList from '../constructor/addMovie.js'
-import createProgram from '../constructor/createProgram.js'
-import addMovieToProgram from '../constructor/addMovieToList.js'
+import addMovieToList from '../functions/addMovie.js'
+import createProgram from '../functions/createProgram.js'
+import addMovieToProgram from '../functions/addMovieToList.js'
 import {createMovieButton, createProgramButtons, addMovieBtnElement } from '../entities/variables.js'
 
 createMovieButton.addEventListener('click', addMovieToList);
